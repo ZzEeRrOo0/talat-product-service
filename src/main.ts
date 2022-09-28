1e0cca05800830d043a3a7d1a5c1df2bb1174eae
ZzEeRrOo0 <noy2015laos@gmail.com>
2022-09-28 23:28:15 +0700

    add api base url constant

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import server from "./server";
 import * as dotenv from "dotenv";
 import { ProductMiddleWare } from "./presentation/routers/middle-ware/middle-ware";
+import { API_BASE_URL } from "../config/constants";
 
 dotenv.config();
 
 (async () => {
 	// server.use("/contact", contactMiddleWare);
-	server.use("/products", ProductMiddleWare);
+	server.use(API_BASE_URL + "/products", ProductMiddleWare);
 	server.listen(process.env.PORT, () => console.log("Running on server"));
 })();
