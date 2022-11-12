4adf614a2957732fa26583bda157e1c8bf4caf9e
ZzEeRrOo0 <noy2015laos@gmail.com>
2022-11-12 11:48:51 +0700

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,26 @@
-import express from 'express'; 
-const server = express(); 
-server.use(express.json()); 
-export default server
+import express from "express";
+import cors from "cors";
+
+//get router
+var router = express.Router();
+
+//options for cors midddleware
+const options: cors.CorsOptions = {
+	allowedHeaders: [
+		"Origin",
+		"X-Requested-With",
+		"Content-Type",
+		"Accept",
+		"X-Access-Token",
+	],
+	credentials: true,
+	methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+	origin: "*",
+	preflightContinue: false,
+};
+
+const server = express();
+server.use(express.json());
+router.use(cors(options));
+router.options("*", cors(options));
+export default server;
