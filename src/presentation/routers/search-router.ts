1e229306b87742066bc26ed51913f11c922f93f2
noykhodtham2023 <noykt.2023@gmail.com>
2023-03-16 16:27:52 +0700

    fix search filter

--- src/presentation/routers/search-router.ts
+++ src/presentation/routers/search-router.ts
@@ -11,8 +11,12 @@ export default function SearchRouter(
 	router.get("/filter-product-name", async (req: Request, res: Response) => {
 		try {
 			const name = req.query["name"]?.toString() ?? "";
-			const data = await getListFilterProductUseCase.execute(name);
-			res.send(new APIResponse(200, data));
+			if (name != "") {
+				const data = await getListFilterProductUseCase.execute(name);
+				res.send(new APIResponse(200, data));
+			} else {
+				res.send(new APIResponse(200, []));
+			}
 		} catch (err) {
 			res.send(new APIResponse(500, { message: "Error fetching data" }));
 		}
